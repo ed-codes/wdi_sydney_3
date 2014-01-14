67f0366378cef6b9846c97957e636dfe54540f9f
Eduard Fastovski <fedd92@gmail.com>
2014-01-14 16:51:42 +1100

    cleaned up subway.rb

--- students/eduard_fastovski/w1d3/subway.rb
+++ students/eduard_fastovski/w1d3/subway.rb
@@ -21,7 +21,7 @@ puts "Which train are you getting off? n, l, s?"
 puts "Which stop are you getting off?"
 	@stopoff = gets.chomp
 
-#find the index of where they got on and where they got off - returns - 
+#returns just the index number where they got on and where they got off 
   index_on = (subway[@line_on].index(@stopon))
   index_off = (subway[@line_off].index(@stopoff))
 
@@ -32,10 +32,11 @@ puts "Which stop are you getting off?"
   # the index of union square on the line they got off
   unionout = subway[@line_off].index("union square")
 
-
-  if @line_on != @line_off # if the person is changing lines
+# if the person is changing lines
+  if @line_on != @line_off 
     
-    @numstops = (unionin - index_on).abs + (unionout - index_off).abs   # abs turns negatives into positives
+    @numstops = (unionin - index_on).abs + (unionout - index_off).abs   
+    # abs turns negatives into positives
     
     array_on = subway[@line_on]
     array_off = subway[@line_off] # the hash it should search through
@@ -45,16 +46,16 @@ puts "Which stop are you getting off?"
 # THE FIRST HALF OF THE TRIP
     #If the index where they got on is smaller than the index of union square, count the stations up to union
     if index_on < unionin
-      index_on.upto(unionin) { |stop| stops_covered<<(array_on[stop]) } # push each stop to the stops_covered array
+      index_on.upto(unionin) { |stop| stops_covered.push(array_on[stop]) } #push each stop to the stops_covered array
     #If the index where they got on is higher than union square, list the stations down to union square
     elsif  index_on > unionin
-      index_on.downto(unionin) { |stop| stops_covered<<(array_on[stop]) }
+      index_on.downto(unionin) { |stop| stops_covered.push(array_on[stop]) }
     end
 # THE SECOND HALF OF THE TRIP
     if index_off > unionout
-      unionout.upto(index_off) { |stop| stops_covered<<(array_off[stop]) }
+      unionout.upto(index_off) { |stop| stops_covered.push(array_off[stop]) }
     elsif  index_off < unionout
-      unionout.downto(index_off) { |stop| stops_covered<<(array_off[stop]) }
+      unionout.downto(index_off) { |stop| stops_covered.push(array_off[stop]) }
     end
 
 #if they are not changing lines
